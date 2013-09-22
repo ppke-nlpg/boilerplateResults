89543f581923dea9291fd4715a21d3cd88afb788
endredy <istvan.endredy@gmail.com>
2013-09-22 22:07:55 +0200

    share these results on github

---
+++ Readme.md
@@ -0,0 +1,20 @@
+
+Boilerplate removal tools results on two dataset: CleanEval and CleanPortalEval
+
+CleanEval source: http://corpus.leeds.ac.uk/cleaneval/gold-24k/ 
+CleanPortalEval source: https://github.com/ppke-nlpg/CleanPortalEval
+
+Tested algorithms:
+ boilerpipe
+ bte
+ goldminer
+ goldminer+onion
+ justext
+ justext+onion
+
+
+Contents:
+cleanEvalResults: results of boilerplate removal algorithms on CleanEval dataset 
+
+cleanPortalEvalResults: results of boilerplate removal algorithms on CleanPortalEval dataset 
+
---
+++ cleanPortalEvalResults/start.sh
@@ -0,0 +1,21 @@
+# cleanEval evaluation of boilerplate algorithms
+#  cleaneval.py was written by Evert, Stefan, http://www.lrec-conf.org/proceedings/lrec2008
+
+echo ====== justext ======
+python cleaneval.py -st justext GoldStandard
+
+echo ====== justext+onion ======
+python cleaneval.py -st justext+onion GoldStandard
+
+echo ====== boilerpipe ======
+python cleaneval.py -st boilerpipe GoldStandard
+
+echo ====== bte ======
+python cleaneval.py -st bte GoldStandard
+
+echo ====== goldminer+onion ======
+python cleaneval.py -st goldminer+onion GoldStandard
+
+echo ====== goldminer+onion ======
+python cleaneval.py -st goldminer+onion GoldStandard
+
